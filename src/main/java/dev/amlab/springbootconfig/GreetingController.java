3be08380f0218d42e1b4e7c0c890cdc839b94ce4
Trinh Vu <txvu100@gmail.com>
2022-03-21 17:05:00 -0500

    Added first var

---
+++ src/main/java/dev/amlab/springbootconfig/GreetingController.java
@@ -0,0 +1,17 @@
+package dev.amlab.springbootconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController
+{
+	@Value("${my.greeting}")
+	private String greetingMessage;
+
+	@GetMapping("/greeting")
+	public String greeting() {
+		return greetingMessage;
+	}
+}
