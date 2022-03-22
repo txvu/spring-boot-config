3382b16e4b1e3e557b66d2cda1be51fb33db7e0c
Trinh Vu <txvu100@gmail.com>
2022-03-21 21:15:17 -0500

    3 more way to ref external values

--- src/main/java/dev/amlab/springbootconfig/GreetingController.java
+++ src/main/java/dev/amlab/springbootconfig/GreetingController.java
@@ -4,14 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController
 {
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default value}")
 	private String greetingMessage;
+
+	@Value("some static message")
+	private String staticMessage;
+
+	@Value("${my.list.values}")
+	private List<String> listValues;
+
+	@Value("#{${dbValues}}")
+	private Map<String, String> dbValues;
 
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + staticMessage + listValues + dbValues;
 	}
 }
